ba61ceb8a66f57418fdf65c6d245e944f8a52984
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-07-08 21:59:37 +0530

    updated vercel.json file

--- db.js
+++ db.js
@@ -5,6 +5,8 @@ dotenv.config();
 const db_connect = async () => {
   const db_URL = process.env.MONGO_DB;
 
+  if(!db_URL)return console.log("MONGO_DB environment variable is not defined");
+
   try {
     await mongoose
       .connect(db_URL)
