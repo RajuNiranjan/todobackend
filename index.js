e2cc4bab4e57c23852fd828097d5c2a0f00f97d1
Syed <syedmujahidahmed0@gmail.com>
2024-07-09 10:13:55 +0530

    changed routing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import "./db.js";
-import { movieRouter } from "./routers/movie.router.js";
+import movieRouter from "./routers/movie.router.js";
 dotenv.config();
 
 const app = express();
